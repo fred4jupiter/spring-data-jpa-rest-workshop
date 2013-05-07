dfb2a2233fa134d63ead2e985b50a095e4d0f66f
michael.staehler <michael.staehler@opitz-consulting.com>
2013-05-07 10:05:45 +0200

    cleanup

--- src/main/java/com/opitzconsulting/springdata/jpa/util/SampleDataProvider.java
+++ src/main/java/com/opitzconsulting/springdata/jpa/util/SampleDataProvider.java
@@ -5,26 +5,21 @@ import com.opitzconsulting.springdata.jpa.domain.Customer;
 import com.opitzconsulting.springdata.jpa.repository.CustomerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
-
+/**
+ * Populates demo data at startup to query some data at runtime.
+ */
 @Component
-public class SampleDataProvider implements ApplicationListener<ContextRefreshedEvent>, ApplicationContextAware {
+public class SampleDataProvider implements ApplicationListener<ContextRefreshedEvent> {
 
     private static final Logger LOG = LoggerFactory.getLogger(SampleDataProvider.class);
 
-    private static final String SQL_FILE = "data.sql";
-
     private final CustomerRepository customerRepository;
-
-    private ApplicationContext applicationContext;
 
     @Autowired
     public SampleDataProvider(CustomerRepository customerRepository) {
@@ -35,32 +30,24 @@ public class SampleDataProvider implements ApplicationListener<ContextRefreshedE
     }
 
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
-
-    @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-        if (!event.getApplicationContext().equals(applicationContext)) {
-            return;
-        }
-
+        Assert.notNull(customerRepository, "CustomerRepository must not be null!");
         LOG.info("populating demo customers...");
         populateDemoCustomers();
     }
 
     private void populateDemoCustomers() {
         Customer customer1 = new Customer(500.00, "Fred", "Feuerstein", "fred@feuerstein.de");
-        Customer customer2 = new Customer(546.34, "Wilma", "Feuerstein", "wilma@feuerstein.de");
-        Customer customer3 = new Customer(12345.00, "Karl", "Katze", "karl@katze.de");
-        Customer customer4 = new Customer(250.00, "Bert", "Bernstein", "bert@bernstein.de");
-        Customer customer5 = new Customer(7596.00, "Elke", "Olle", "elke@olle.de");
-
         customer1.addAddress(new Address("Milchstrasse", "Hamburg", "Deutschland"));
+        saveCustomer(customer1);
+
+        saveCustomer(new Customer(546.34, "Wilma", "Feuerstein", "wilma@feuerstein.de"));
+        saveCustomer(new Customer(12345.00, "Karl", "Katze", "karl@katze.de"));
+        saveCustomer(new Customer(250.00, "Bert", "Bernstein", "bert@bernstein.de"));
+        saveCustomer(new Customer(7596.00, "Elke", "Olle", "elke@olle.de"));
+    }
+
+    private void saveCustomer(Customer customer1) {
         customerRepository.saveAndFlush(customer1);
-        customerRepository.saveAndFlush(customer2);
-        customerRepository.saveAndFlush(customer3);
-        customerRepository.saveAndFlush(customer4);
-        customerRepository.saveAndFlush(customer5);
     }
 }

46c7918aa5f8f1badbd92c4cc2f2de94616f2be8
michael.staehler <michael.staehler@opitz-consulting.com>
2013-05-07 10:47:52 +0200

    added AddressRepository

--- src/main/java/com/opitzconsulting/springdata/jpa/repository/AddressRepository.java
+++ src/main/java/com/opitzconsulting/springdata/jpa/repository/AddressRepository.java
@@ -1,10 +1,10 @@
 package com.opitzconsulting.springdata.jpa.repository;
 
-import com.opitzconsulting.springdata.jpa.domain.Customer;
+
+import com.opitzconsulting.springdata.jpa.domain.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.repository.annotation.RestResource;
 
-@RestResource(rel = "customers", path = "customers")
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+@RestResource(rel = "addresses", path = "addresses")
+public interface AddressRepository extends JpaRepository<Address, Long> {
 }
